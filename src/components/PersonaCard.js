5911c7e2af6f56e614a8224d3fba8cd271e54f63
Arun Menon <arunmenon@Aruns-MacBook-Pro.local>
2023-08-06 11:46:28 +0530

    added file upload for chat transcripts

--- src/components/PersonaCard.js
+++ src/components/PersonaCard.js
@@ -1,16 +1,60 @@
-import React from 'react';
-import { Card, CardContent, Typography, LinearProgress } from '@mui/material';
+import React, { useState } from 'react';
+import { Card, CardContent, Typography, LinearProgress, Button, TextField } from '@mui/material';
 
-export default function PersonaCard({ title, score }) {
+export default function PersonaCard({ id, title, score, description, onDescriptionUpdate }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedDescription, setEditedDescription] = useState(description);
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleCardClick = () => {
+    setIsFlipped(!isFlipped);
+  };
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+  }
+
+  const handleSaveClick = () => {
+    setIsEditing(false);
+    onDescriptionUpdate(id, editedDescription);
+  };
+
+  const handleDescriptionChange = (e) => {
+    setEditedDescription(e.target.value);
+  }
+
   return (
-    <Card style={{ height: '100%' }} variant="outlined">
-      <CardContent>
-        <Typography variant="h6">{title}</Typography>
-        <div style={{ marginTop: '15px', marginBottom: '10px' }}>
-          <LinearProgress variant="determinate" value={score} />
-        </div>
-        <Typography variant="caption">Score: {score}%</Typography>
-      </CardContent>
-    </Card>
+    <div onClick={handleCardClick} style={{ perspective: '1000px', width: '150px', height: '200px' }}>
+      <div style={{ transformStyle: 'preserve-3d', transition: 'transform 0.6s', transform: isFlipped ? 'rotateY(180deg)' : 'none' }}>
+        <Card style={{ position: 'absolute', backfaceVisibility: 'hidden' }}>
+          <CardContent>
+            <Typography variant="h6">{title}</Typography>
+            <LinearProgress variant="determinate" value={score} style={{ marginTop: '15px' }} />
+            <Typography variant="caption" style={{ marginTop: '10px' }}>Score: {score}%</Typography>
+          </CardContent>
+        </Card>
+        <Card style={{ position: 'absolute', backfaceVisibility: 'hidden', transform: 'rotateY(180deg)' }}>
+          <CardContent>
+            {isEditing ? (
+              <>
+                <TextField
+                  fullWidth
+                  variant="outlined"
+                  label="Edit Description"
+                  value={editedDescription}
+                  onChange={handleDescriptionChange}
+                />
+                <Button onClick={handleSaveClick} style={{ marginTop: '10px' }}>Save</Button>
+              </>
+            ) : (
+              <>
+                <Typography variant="body2">{description}</Typography>
+                <Button onClick={handleEditClick} style={{ marginTop: '10px' }}>Edit</Button>
+              </>
+            )}
+          </CardContent>
+        </Card>
+      </div>
+    </div>
   );
 }
--- src/components/PersonaManagement.js
+++ src/components/PersonaManagement.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Tabs, Tab, Grid, Paper, TextField, Pagination, Typography } from '@mui/material';
+import {Button, Tabs, Tab, Grid, Paper, TextField, Pagination, Typography } from '@mui/material';
 import personas from '../data/CustomerPersonas';
 import PersonaCard from '../components/PersonaCard';
 
@@ -7,19 +7,62 @@ export default function PersonaManagement() {
   const [value, setValue] = React.useState(0);
   const [searchTerm, setSearchTerm] = React.useState("");
   const [page, setPage] = React.useState(1);
-  const itemsPerPage = 5;
+  const itemsPerPage = 3;
+  const [file, setFile] = React.useState(null);
+
+
+  // Move personas data to local state
+  const [personasData, setPersonasData] = React.useState(personas);
+
+  // Handle updating a persona description
+  const handleDescriptionUpdate = (id, newDescription) => {
+    const updatedPersonas = personasData.map(persona => 
+      persona.id === id ? { ...persona, description: newDescription } : persona
+    );
+    setPersonasData(updatedPersonas);
+  }
+  const handleFileChange = (event) => {
+    const uploadedFile = event.target.files[0];
+    setFile(uploadedFile);
+};
+
+const generatePersonas = () => {
+    if (!file) return;
+    
+    // Here, you can process the uploaded file and generate personas.
+    // This is just a placeholder and will need to be implemented based on your requirements.
+    console.log(`Generating personas from ${file.name}...`);
+    // After processing, reset the file.
+    setFile(null);
+};
+
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
-    // Scroll to the content container
+    
     const contentElement = document.getElementById("personaContentContainer");
-    if (contentElement) {
+    if (!contentElement) return;
+
+    // Scroll to the content container when "View Personas" is selected.
+    if (newValue === 0) {
       window.scrollTo({
         top: contentElement.offsetTop,
         behavior: "smooth"
       });
+    } 
+    // Scroll to the top of the Paper component when "Generate Personas" is selected.
+    else if (newValue === 1) {
+      const paperElement = contentElement.closest('paper'); // Assuming your Paper component generates a 'paper' tag.
+      if (paperElement) {
+        window.scrollTo({
+          top: paperElement.offsetTop,
+          behavior: "smooth"
+        });
+      }
     }
-  };
+};
+
+
 
   const filteredPersonas = personas.filter(
     persona => persona.title.toLowerCase().includes(searchTerm.toLowerCase())
@@ -59,9 +102,11 @@ export default function PersonaManagement() {
               {filteredPersonas.slice((page - 1) * itemsPerPage, page * itemsPerPage).map((persona) => (
                 <Grid item xs={12} sm={6} md={4} lg={3} key={persona.id}>
                   <PersonaCard 
+                    id={persona.id}
                     title={persona.title} 
                     description={persona.description} 
-                    score={persona.score} 
+                    score={persona.score}
+                    onDescriptionUpdate={handleDescriptionUpdate} 
                   />
                 </Grid>
               ))}
@@ -76,10 +121,36 @@ export default function PersonaManagement() {
         )}
         
         {value === 1 && (
-          <div>
-            {/* Code to allow users to upload chat transcripts and generate personas */}
-          </div>
-        )}
+    <div style={{ padding: '20px' }}>
+        <Typography variant="h6" style={{ marginBottom: '20px', textAlign: 'center' }}>
+            Generate Personas from Chat Transcripts
+        </Typography>
+        <input
+            accept=".txt" // Accepting only .txt files for simplicity, you can modify this.
+            id="upload-chat-transcripts"
+            type="file"
+            style={{ display: 'none' }}
+            onChange={handleFileChange}
+        />
+        <label htmlFor="upload-chat-transcripts">
+            <Button variant="contained" color="primary" component="span">
+                Upload Chat Transcripts
+            </Button>
+        </label>
+        {file && <Typography variant="body1" style={{ marginTop: '20px' }}>File uploaded: {file.name}</Typography>}
+        <Button 
+            variant="contained" 
+            color="secondary" 
+            style={{ marginTop: '20px' }} 
+            onClick={generatePersonas}
+            disabled={!file}
+        >
+            Generate Personas
+        </Button>
+    </div>
+)}
+
+
       </div>
     </Paper>
   );
